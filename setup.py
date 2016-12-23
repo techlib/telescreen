01f354ea0747fd276329f9d6bfcb196768833e6d
Jan Dvořák <jan.dvorak@singularita.net>
2016-12-23 13:13:07 +0100

    Clean up setup.py and associated files
    Signed-off-by: Jan Dvořák <jan.dvorak@singularita.net>

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3 -tt
+# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import os.path
+from os.path import *
+
+def read_requires(path=None):
+    if path is None:
+        path = join(dirname(__file__), 'requirements.txt')
+        print(path)
+
+    with open(path) as fp:
+        return [l.strip() for l in fp.readlines()]
 
 setup(
     name = 'telescreen',
@@ -9,7 +18,7 @@ setup(
     author = 'NTK',
     description = ('Digital signage player for GNOME'),
     license = 'MIT',
-    keywords = 'video image slideshow',
+    keywords = 'video image slideshow indoktrinator',
     url = 'http://github.com/techlib/telescreen',
     include_package_data = True,
     packages = find_packages(),
@@ -17,10 +26,8 @@ setup(
         'License :: OSI Approved :: MIT License',
     ],
     scripts = ['bin/telescreen'],
-    install_requires = [
-    ],
+    install_requires = read_requires(),
 )
 
 
 # vim:set sw=4 ts=4 et:
-# -*- coding: utf-8 -*-
