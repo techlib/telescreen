f6125848edec84a4e40203ef04354104f18306a1
Jan Dvořák <jan.dvorak@singularita.net>
2017-03-08 15:16:19 +0100

    cec: minor style adjustments
    Signed-off-by: Jan Dvořák <jan.dvorak@singularita.net>

--- telescreen/cec.py
+++ telescreen/cec.py
@@ -3,6 +3,7 @@
 
 import os
 import re
+
 from twisted.internet.task import LoopingCall
 from twisted.python import log
 from twisted.internet import reactor
@@ -12,8 +13,8 @@ from twisted.internet.protocol import ProcessProtocol
 
 __all__ = ['CEC']
 
-class CECProtocol(ProcessProtocol):
 
+class CECProtocol(ProcessProtocol):
     def __init__(self, command, callback=log.msg):
         self.command = command
         self.callback = callback
@@ -25,11 +26,12 @@ class CECProtocol(ProcessProtocol):
     def outReceived(self, data):
         self.callback(data)
 
+
 class CEC(object):
     """
     TV power control using HDMI sub-protocol CEC.
     """
-    
+
     def __init__(self):
         self.status = 'unknown'
 
@@ -38,20 +40,21 @@ class CEC(object):
         self.status_loop.start(15)
 
     def set_power_status(self, status):
-        command = '{} 0'.format(status)
-        protocol = CECProtocol(command)
-        reactor.spawnProcess(protocol, which('cec-client')[0], args=['cec-client', '-s', '-d', '1'])
+        protocol = CECProtocol('{} 0'.format(status))
+        reactor.spawnProcess(protocol, which('cec-client')[0],
+                             args=['cec-client', '-s', '-d', '1'])
 
     def query_power_status(self):
-        command = 'pow 0'
-        protocol = CECProtocol(command, self._parse_power_status)
-        reactor.spawnProcess(protocol, which('cec-client')[0], args=['cec-client', '-s', '-d', '1'])
+        protocol = CECProtocol('pow 0', self._parse_power_status)
+        reactor.spawnProcess(protocol, which('cec-client')[0],
+                             args=['cec-client', '-s', '-d', '1'])
 
     def _parse_power_status(self, data):
         try:
-            self.status = re.match('power status: (.*)', data.decode('utf-8')).group(1)
+            m = re.match('power status: (.*)', data.decode('utf-8'))
+            self.status = m.group(1)
         except AttributeError:
             pass
 
-#
+
 # vim:set sw=4 ts=4 et:
