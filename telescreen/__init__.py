0eca9c76187e3a13e598d8e4cff9771db93537ac
Jan Dvořák <jan.dvorak@singularita.net>
2017-03-08 19:08:05 +0100

    Clean up the main module structure
    Signed-off-by: Jan Dvořák <jan.dvorak@singularita.net>

--- telescreen/__init__.py
+++ telescreen/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3 -tt
 # -*- coding: utf-8 -*-
 
-from telescreen.screen import Screen
-from telescreen.manager import Manager
-
-__all__ = ['Screen', 'Manager']
+pass
 
 # vim:set sw=4 ts=4 et:
